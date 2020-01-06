78bf0480b2c5aa424e943fac234510fe257c7e34
Aditya <adityazope261@gmail.com>
2020-01-06 15:30:22 +0530

    Corrected express static path

--- server/src/bin/www.js
+++ server/src/bin/www.js
@@ -9,7 +9,7 @@ import Database from '../database/database';
 
 const app = express();
 
-const DIST_DIR = path.join('../../../', 'client', 'dist');
+const DIST_DIR = path.join(__dirname, '../../../', 'client', 'dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 app.use(express.static(DIST_DIR));
 
